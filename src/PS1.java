287367c68ca2acea7002cb707d3b6668c5a812de
Ayushi Gupta <ayushigupta36881@gmail.com>
2025-10-15 14:47:20 +0530

    PS1

---
+++ src/PS1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class PS1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        long n = input;
+        while(n != 1){
+            System.out.print(n + " ");
+            if(n % 2 == 0){
+                n /= 2;
+            }
+            else{
+                n = (n * 3) + 1; // huge int: * 3: huge num: out of range of Int: overflow: -ve
+            }
+        }
+        System.out.print(1);
+    }
+}
